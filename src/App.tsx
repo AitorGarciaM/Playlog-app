026c1bcec8dbbfd6277b6bf87cee498bdf3b88cd
laiarova <86849402+laiarova@users.noreply.github.com>
2025-11-17 19:03:15 +0100

    añadir traducciones i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useTranslation } from 'react-i18next'
 
 function App() {
   const [count, setCount] = useState(0)
-
+    const { t } = useTranslation();
   return (
     <>
+        <p>{t("welcome")}</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
