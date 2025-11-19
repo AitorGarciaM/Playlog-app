51251e728f8bab905c1af96e7b8a701b99b8d3ec
Aitor <jgaitor@msn.com>
2025-11-19 20:30:04 +0100

    merge commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import './App.css'
+
+import Navbar from './components/Navbar';
+import { useTranslation } from 'react-i18next'
+import { useTheme } from "./hooks/useTheme";
 
 function App() {
-  return (
+    const { t } = useTranslation();
+    const { toggleTheme } = useTheme();
+    const myColor = getComputedStyle(document.documentElement)
+        .getPropertyValue("--background")
+        .trim();
+    return (
     <>
+        <Navbar />
+        <p>{t("welcome")}</p>
+        <button style={{background: myColor}} onClick={toggleTheme}>Cambiar tema</button>
     </>
   )
 }
